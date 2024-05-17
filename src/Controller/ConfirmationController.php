6fb41d968e0d5626555d720abf5a6c4f0661b1bb
victor <153332920+victoraubague1@users.noreply.github.com>
2024-05-17 13:48:00 +0200

    CSS

--- src/Controller/ConfirmationController.php
+++ src/Controller/ConfirmationController.php
@@ -4,15 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ConfirmationController extends AbstractController
 {
     #[Route('/confirmation', name: 'app_confirmation')]
     public function index(): Response
     {
+        // Path to the PDF directory
+        $pdfDir = $this->getParameter('kernel.project_dir') . '/public/pdf';
+
+        // Get all PDF files from the directory
+        $pdfFiles = glob($pdfDir . '/*.pdf');
+
+        // Extract just the filenames
+        $pdfFilenames = array_map('basename', $pdfFiles);
+
         return $this->render('confirmation/index.html.twig', [
-            'controller_name' => 'ConfirmationController',
+            'pdfFiles' => $pdfFilenames,
         ]);
     }
 }
